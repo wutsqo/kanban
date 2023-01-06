8fe2a2473041863ea06767bde8bf36f45e9efeb1
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-01-06 23:49:43 +0700

    chore: setup redirect rule
    - redirect from "/" to "/v1"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/v1",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
