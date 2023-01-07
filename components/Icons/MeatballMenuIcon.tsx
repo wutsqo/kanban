c26aef0505b68dfc37ebc46295d1f6fc03412923
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-01-07 09:16:45 +0700

    fix: fix invalid dom property on icons

--- components/Icons/MeatballMenuIcon.tsx
+++ components/Icons/MeatballMenuIcon.tsx
@@ -11,7 +11,7 @@ export const MeatBallMenuIcon = () => {
         d="M19 13C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11C18.4477 11 18 11.4477 18 12C18 12.5523 18.4477 13 19 13Z"
         stroke="#757575"
         stroke-width="2"
-        stroke-linecap="round"
+        strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
