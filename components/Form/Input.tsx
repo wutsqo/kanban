4e6fcb2f5869bb0247833478cbd897689a74ec4c
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-01-07 08:09:12 +0700

    feat: add input component

---
+++ components/Form/Input.tsx
@@ -0,0 +1,38 @@
+import { forwardRef } from "react"
+
+interface Props {
+  label: string
+  name: string
+  type: string
+  placeholder: string
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  step?: number
+  min?: number
+  max?: number
+}
+
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ label, name, type, placeholder, value, onChange, ...props }, ref) => {
+    return (
+      <div className="w-full flex flex-col gap-3 items-start mt-6">
+        <label htmlFor={name} className="text-xs font-bold">
+          {label}
+        </label>
+        <input
+          ref={ref}
+          id={name}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className="border-2 border-[#EDEDED] px-4 py-2 rounded-md focus:outline-none w-full"
+          {...props}
+        />
+      </div>
+    )
+  }
+)
+
+Input.displayName = "Input"
